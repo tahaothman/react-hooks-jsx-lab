b946e5147150e82e3d2440f03b4b8b73bf22ff00
Taha <taha.othman@outlook.com>
2023-05-22 11:58:16 -0400

    complete the lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,13 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
+  const firebrick = {
+    color: 'firebrick',
+  };
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (<div id= "home">
+    <h1 style={firebrick}>{name} is a Web Developer from {city}</h1>
+  </div>);
 }
 
 export default Home;
